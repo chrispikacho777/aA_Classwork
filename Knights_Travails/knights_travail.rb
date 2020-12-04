c5438cc00b838624ffd661dd4fe05d1df441cbe4
chris <chrispikacho@gmail.com>
2020-12-04 15:42:09 -0800

    Added valid_moves method

--- Knights_Travails/knights_travail.rb
+++ Knights_Travails/knights_travail.rb
@@ -2,16 +2,18 @@ require '00_tree_node.rb'
 
 class KnightPathFinder
 
+    MOVES = [[2,1], [2,-1], [-2,1], [-2,-1], [1,2], [1,-2], [-1,2], [-1,-2]]
 
     def self.valid_moves(pos)
         x, y = pos
         valid_pos = []
-        root = PolyTreeNode.new(pos)
-        root.add_children
-
-
-
-
+        MOVES.each do |shift|
+            new_x = x+shift[0]
+            new_y = y+shift[1]
+            valid_pos << [new_x, new_y] if new_x.between?(0,7) && new_y.between?(0,7) && !considered_positions.include?([new_x, new_y])
+        end
+        @considered_positions += valid_pos
+        valid_pos
     end
 
     def initialize(pos)
