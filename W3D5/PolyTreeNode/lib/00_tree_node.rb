3cf7c1953cc4045506bc9c51c17590094fd5eda3
chris <chrispikacho@gmail.com>
2020-12-04 12:08:05 -0800

    Added initialize method and started parent= method

--- W3D5/PolyTreeNode/lib/00_tree_node.rb
+++ W3D5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
+    
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(name)
+        @parent = name
+        name.children << self if !@name.children.include?(self) && name != nil
+    end
 
 end
