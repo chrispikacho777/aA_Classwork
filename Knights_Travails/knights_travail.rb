1821a19055535dd473de0ed779030a0b6c33ffdf
Janette <tinoconjanette@gmail.com>
2020-12-04 16:22:59 -0800

    Add New_move_positions method

--- Knights_Travails/knights_travail.rb
+++ Knights_Travails/knights_travail.rb
@@ -1,4 +1,4 @@
-require '00_tree_node.rb'
+require "./../../00_tree_node.rb"
 
 class KnightPathFinder
 
@@ -10,18 +10,21 @@ class KnightPathFinder
         MOVES.each do |shift|
             new_x = x+shift[0]
             new_y = y+shift[1]
-            valid_pos << [new_x, new_y] if new_x.between?(0,7) && new_y.between?(0,7) && !considered_positions.include?([new_x, new_y])
+            valid_pos << [new_x, new_y] if new_x.between?(0,7) && new_y.between?(0,7) 
         end
-        @considered_positions += valid_pos
         valid_pos
     end
 
     def initialize(pos)
         @root_node = pos
         @paths = PolyTreeNode.new(@root_node)
-        @considered_positions = [] 
+        @considered_positions = [pos] 
     end
     
-    def new_move_positions
+    def new_move_positions(pos)
+        all_moves = KnightPathFinder.valid_moves(pos)
+        new_move = all_moves.select{|move| !considered_positions.include?(move)}
+        @considered_positions += new_move
+        new_move
     end
 end    
