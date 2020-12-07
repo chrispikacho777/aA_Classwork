3575c0a9a5aaa2b95e0ca93a70cf3202e13ed927
Janette <tinoconjanette@gmail.com>
2020-12-07 15:32:35 -0800

    Finished Tic-Tac-Toe

--- W4D1/skeleton/lib/super_computer_player.rb
+++ W4D1/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,17 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    root_node = TicTacToeNode.new(game.board, mark)
+    child_nodes = root_node.children
+    child_nodes.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark) 
+    end
+
+    child_nodes.each do |child|
+      return child.prev_move_pos if !child.losing_node?(mark)
+    end
+
+    raise "No non-losing node found"
   end
 end
 
