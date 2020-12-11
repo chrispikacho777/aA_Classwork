f315f11440aeb6ec2fff6db3ea65e21e3f72ebcb
Dmitrii An <an.dmitriy93@gmail.com>
2020-12-11 11:27:03 -0800

    time differences is done

--- W4D5/time_diff.rb
+++ W4D5/time_diff.rb
@@ -22,3 +22,36 @@ end
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min_1(list) 
 
+
+def sub_sum_1(list) # time complexity is O(n^2)
+    sub_arr = []
+    (0...list.length - 1).each do |i|
+        (i...list.length).each do |j|
+            sub_arr << list[i..j]
+        end
+    end
+    sub_arr.inject { |arr1, arr2| arr2.sum > arr1.sum ? arr1 = arr2 : arr1 }.sum
+end
+
+def sub_sum(list) # time complexity is O(n)
+    largest_sum = 0
+    current_sum = 0
+
+    list.each do |num|
+        current_sum += num
+        if current_sum >= largest_sum
+            largest_sum = current_sum
+        else
+            current_sum = num
+        end
+    end
+    largest_sum
+end
+
+
+
+
+
+list = [5, 3, -7]
+# p sub_sum_1(list)
+p sub_sum(list)
