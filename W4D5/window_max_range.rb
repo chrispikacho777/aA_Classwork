bd4be95785ce041dd88482397912e2de44ab3fbc
Dmitrii An <an.dmitriy93@gmail.com>
2020-12-11 16:02:48 -0800

    windowed_max_range :MyQueue is done

--- W4D5/window_max_range.rb
+++ W4D5/window_max_range.rb
@@ -1,4 +1,4 @@
-def windowed_max_range(arr, size)
+def windowed_max_range(arr, size) # O(n^2)
     current_max_range = nil
 
     (0...arr.length).each do |i|
@@ -10,7 +10,42 @@ def windowed_max_range(arr, size)
     current_max_range
 end
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 2) #== 4 # 4, 8
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 3) #== 5 # 0, 2, 5
+# p windowed_max_range([1, 0, 2, 5, 4, 8], 4) #== 6 # 2, 5, 4, 8
+# p windowed_max_range([1, 3, 2, 5, 4, 8], 5) #== 6 # 3, 2, 5, 4, 8
+
+
+class MyQueue
+    def initialize
+      @store = []
+    end
+    
+    def peek
+        @store[0]
+    end
+
+    def size
+        @store.length
+    end
+
+    def empty?
+        @store.empty?
+    end
+    
+    def enqueue(input)
+        @store << input
+    end
+
+    def dequeue
+        @store.shift
+    end
+end
+
+
+class MyStack
+    def initialize
+      @store = []
+    end
+end
+
