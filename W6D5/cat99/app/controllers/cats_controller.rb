6938459c57b3d73f4b20e88727f6c042b66bc5fb
chris <chrispikacho@gmail.com>
2020-12-31 16:48:37 -0800

    finished edit erb

--- W6D5/cat99/app/controllers/cats_controller.rb
+++ W6D5/cat99/app/controllers/cats_controller.rb
@@ -17,8 +17,37 @@ class CatsController < ApplicationController
     render :new 
   end
 
-  def edit 
-    
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
   end
   
+
+  def edit
+    @cat = Cat.find(params[:id])
+    
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+  
+
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :color, :description, :sex)
+  end
 end
