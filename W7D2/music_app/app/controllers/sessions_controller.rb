7bf5870798e36266be1a5ce229cb2bcaee380a64
chris <chrispikacho@gmail.com>
2021-01-05 11:40:36 -0800

    Finished all controllers and views

--- W7D2/music_app/app/controllers/sessions_controller.rb
+++ W7D2/music_app/app/controllers/sessions_controller.rb
@@ -1,26 +1,25 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
     render :new
   end
 
   def create
-    user = User.find_by_credentials(params[:user][:email],
+    @user = User.find_by_credentials(params[:user][:email],
                                      params[:user][:password])
-    if user.nil?
+    if @user.nil?
       flash.now[:errors] = ["Invalid username or password"]
+      @user = User.new({email: params[:user][:email], password: params[:user][:password]})
       render :new
     else
-      # login(user)
-      redirect_to user_url(user)
+      login!(@user)
+      redirect_to user_url(@user)
     end 
   end
 
   def destroy
-    #logout
-    current_user.reset_session_token!
-    session[:session_token] = nil
-    @current_user = nil
+    logout!
     redirect_to new_session_url
   end
 end
